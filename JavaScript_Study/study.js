1e90854d28d51b522ff22549fe1b595d0b3c0d3b
sp0nd <sp0nd@naver.com>
2020-10-27 13:20:44 +0900

    inirial commit

---
+++ JavaScript_Study/study.js
@@ -0,0 +1,31 @@
+var msg='hello world'; 
+console.log(msg);
+
+var a =10;
+console.log(a);
+var b = 3.141592;
+console.log(b);
+
+var as = "우리나라\n";
+console.log(as);
+var bs = "\t대한민국";
+console.log(bs);
+
+console.log(typeof(10));
+//숫자
+console.log(typeof("대한민국"));
+//문자열
+console.log(typeof({}));
+//객체
+console.log(typeof(num));
+//정의되지 않은 변수
+console.log(typeof(function(){}));
+//함수
+console.log(typeof(true));
+//trur/false
+
+Number() // 데이터 타입을 숫자로 변환
+Boolean() // 0이거나 NULL이 아니면 다 트루
+String() // 데이터 타입을 스트링으로 변환
+
+    
